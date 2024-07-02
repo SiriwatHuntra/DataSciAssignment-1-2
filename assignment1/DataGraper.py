51ad71292ee2915b89fef470f79c3a9c5a1e2d2e
[Sirirwat] <siriwat26room1@gmail.com>
2024-07-02 20:04:18 +0700

    add code from ipynb

---
+++ assignment1/DataGraper.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from pandas.api.types import is_numeric_dtype
+
+data = pd.read_csv('http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',header=None)
+data.columns = ['sepal length', 'sepal width', 'petal length', 'petal width', 'class']
+
+data.head()
+
+for col in data.columns:
+    if is_numeric_dtype(data[col]):
+        print('%s:' % (col))
+        print('\t Mean = %.2f' % data[col].mean())
+        print('\t Standard deviation = %.2f' % data[col].std())
+        print('\t Minimum = %.2f' % data[col].min())
+        print('\t Maximum = %.2f' % data[col].max())
+
+# data['class'].value_counts()
+# data.describe(include='all')
+
+# print('Covariance:')
+# data.cov()
+
+# print('Correlation:')
+# data.corr()
